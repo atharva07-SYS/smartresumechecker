90a022c9753cd40cafc5bac68917515f564443ad
Atharva <atharvapadalghare@gmail.com>
2025-07-02 15:15:15 +0530

    Force clean NLTK data download to /tmp/nltk_data for Render

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,12 @@
 from flask import Flask
 import os
 import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+nltk_data_dir = '/tmp/nltk_data'
+os.makedirs(nltk_data_dir, exist_ok=True)
+nltk.data.path.clear()
+nltk.data.path.append(nltk_data_dir)
+nltk.download('punkt', download_dir=nltk_data_dir, force=True)
+nltk.download('stopwords', download_dir=nltk_data_dir, force=True)
 
 def create_app():
     app = Flask(__name__)
